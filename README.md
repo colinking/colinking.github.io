6b98329390cfe1cb90b2cf1dcc5c6b1cb05ed6b5
Colin King <colin.king.96@gmail.com>
2015-08-24 17:01:36 -0400

    2.0 Redesign, Project-Oriented Focus

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Colin King's Website
+# Colin King, Personal Website, v2.0
+This website is built to be project-oriented. You are welcome to use and modify this website under the GPL license.
 
-This is the website for Colin King. 
+# Have an error?
+- "500 Error: secret option required for sessions": Set the SESSION_SECRET variable in your environment (it can be anything, just make it something unique)
---
+++ app.js
@@ -0,0 +1,93 @@
+'use strict'
+
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+var compress = require('compression');
+//var favicon = require('serve-favicon');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
+var lusca = require('lusca');
+var path = require('path');
+var assets = require('connect-assets');
+var sass = require('node-sass');
+
+/**
+ * Controllers (route handlers).
+ */
+var homeController = require('./controllers/home');
+
+/**
+ * API keys.
+ */
+var secrets = require('./config/secrets.conf');
+
+/**
+ * Extra data
+ */
+var settings = require('./config/settings.conf');
+
+/**
+ * Create Express server.
+ */
+var app = express();
+
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(compress());
+app.use(assets({
+  paths: ['public/css', 'public/js']
+}));
+app.use(logger('dev'));
+//app.use(favicon(path.join(__dirname, 'public/favicon.png')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: secrets.sessionSecret
+}));
+app.use(lusca({
+  csrf: true,
+  xframe: 'SAMEORIGIN',
+  xssProtection: true
+}));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+
+/**
+ * Merge default options into the locals variable.
+ */
+app.use(function(req, res, next) {
+	res.locals.name = "Colin King";
+  res.locals.tagdata = settings.tagdata;
+  res.locals.projects = settings.projects;
+	next();
+});
+
+/**
+ * Primary app routes.
+ */
+app.get('/', homeController.index);
+
+/**
+ * Error Handler.
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+module.exports = app;
+
+
